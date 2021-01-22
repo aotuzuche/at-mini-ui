0c6ff48578593f3748b87915e65b5e5879443438
liuxingyun <liuxingyun2010@gmail.com>
2021-01-22 15:54:12 +0800

    feat: 🎸 优化打包

---
+++ components/index.tsx
@@ -0,0 +1,39 @@
+export { default as AmActionSheet } from './action-sheet'
+
+export { default as AmAlert } from './alert'
+
+export { default as AmAvatar } from './avatar'
+
+export { default as AmButton } from './button'
+
+export { default as AmCard } from './card'
+
+export { default as AmDivider } from './divider'
+
+export { default as AmIndexes } from './indexes'
+
+export { default as AmInput } from './input'
+
+export { default as AmList } from './list'
+
+export { default as AmListItem } from './list-item'
+
+export { default as AmLoadMore } from './load-more'
+
+export { default as AmLoading } from './loading'
+
+export { default as AmModal } from './modal'
+
+export { default as AmPopup } from './popup'
+
+export { default as AmRadio } from './radio'
+
+export { default as AmSearchBar } from './search-bar'
+
+export { default as AmSpin } from './spin'
+
+export { default as AmSwitch } from './switch'
+
+export { default as AmTabs } from './tabs'
+
+export { default as AmToast } from './tabs'
