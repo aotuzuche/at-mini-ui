3c0a4c00d6f476d151c03ed3ab2f49a2345398ab
liuxingyun <liuxingyun2010@gmail.com>
2021-03-15 17:00:23 +0800

    feat: 🎸 add NoData component

---
+++ components/no-data/index.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from '@tarojs/components'
+import cn from 'classnames'
+import React from 'react'
+
+interface IProps {
+  className?: string
+}
+
+const AmList: Taro.FC<IProps> = (props) => {
+  const { className, children = '暂无数据' } = props
+  const classname = cn('am-nodata', className)
+
+  return (
+    <View className={classname}>
+      <View className="w">
+        <View className="icon" />
+        <Text className="text">{children}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default AmList
