c99d782eaa40680b7ae421f2fd3c89ff59854053
liuxingyun <liuxingyun2010@gmail.com>
2021-03-19 17:13:03 +0800

    fix: 🐛 去掉组件的addGlobalClass

--- components/popup/index.tsx
+++ components/popup/index.tsx
@@ -5,6 +5,7 @@ import AmButton from '../button'
 
 interface IProps {
   visible?: boolean
+  children: any
   className?: string
   direction?: 'top' | 'left' | 'right' | 'bottom'
   transparent?: boolean
